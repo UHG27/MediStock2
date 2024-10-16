e41939485d977488173534fcccf6dcc84524d365
luboni <guadaboni03@gmail.com>
2024-10-15 22:55:05 -0600

    Creacion de API

--- apps/ventas/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-# from .models import ventas
-
-# Create your views here.
-def ventas(request):
-    ventas = Sale.objects.all()
-    return render(request, 'templates/ventas/ventas.html', {'sales': sales}) 
